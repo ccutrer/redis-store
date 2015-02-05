72db36c56c6563fc65f213dcf8a1b77ddd22d1bb
Cody Cutrer <cody@instructure.com>
2015-02-05 15:18:14 -0700

    fix setex marshalling for distributed store

--- lib/redis/distributed_store.rb
+++ lib/redis/distributed_store.rb
@@ -34,6 +34,10 @@ class Redis
       node_for(key).setnx(key, value, options)
     end
 
+    def setex(key, expiry, value, options = nil)
+      node_for(key).setex(key, expiry, value, options)
+    end
+
     private
       def _extend_namespace(options)
         @namespace = options[:namespace]
